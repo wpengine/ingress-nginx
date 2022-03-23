c48d5479502cbf5489b0c6e2d1ff9d87184e2184
Freddy Esteban Perez <fredstban@icloud.com>
2022-03-23 15:22:12 -0500

    fix comment

--- rootfs/etc/nginx/lua/balancer.lua
+++ rootfs/etc/nginx/lua/balancer.lua
@@ -167,7 +167,7 @@ local function sync_backends()
       local backend_with_external_name = util.deepcopy(new_backend)
       backends_with_external_name[backend_with_external_name.name] = backend_with_external_name
     else
-      -- If backends_with_external_name contains this backend, otherwise cached backend will be used.
+      -- If backends_with_external_name contains this backend, remove it, otherwise cached backend will be used.
       if backends_with_external_name[new_backend.name] then
         backends_with_external_name[new_backend.name] = nil
       end
